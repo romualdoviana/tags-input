d1fdf6e1012dd7b3d280e701802ed6f7d31c96c6
Romualdo Viana <romualdo.viana@ufpr.br>
2024-08-19 22:37:48 -0300

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<script>
+export default {
+  data: () => ({
+    jsFrameworks: ["vue", "react", "angular"],
+    backendFrameworks: ["Laravel"],
+  }),
+
+  methods: {
+    handleChange(tags) {
+      this.jsFrameworks = [...tags];
+    },
+  },
+};
+</script>
+
+<template>
+  <h1>Your favorite frameworks</h1>
+
+  <div>{{ jsFrameworks }}</div>
+
+  <tags-input :selected-tags="jsFrameworks" @change="handleChange" />
+
+  <div>{{ backendFrameworks }}</div>
+
+  <tags-input
+    :selected-tags="backendFrameworks"
+    @change="backendFrameworks = [...$event]"
+  />
+</template>
+
+<style>
+#app {
+  max-width: 1280px;
+  /* margin: 0 auto; */
+  /* padding: 2rem; */
+  font-family: Inter, system-ui, Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: rgba(5, 5, 5, 0.87);
+  margin-top: 60px;
+}
+h1 {
+  color: blue;
+}
+</style>
---
+++ src/components/TagsInput.vue
@@ -0,0 +1,111 @@
+<template>
+  
+  <div class="tags-input-wrapper">
+    <span class="tag-item" v-for="(tag, index) in tags" :key="index" style="color: black">
+      {{ index + " : " + tag }}
+      <a class="remove-tag" @click.prevent="removeTag(index)" href="#">&times;</a>
+    </span>
+    <hr />
+    <input
+    class="tag-input"
+      type="text"
+      placeholder="Add a tag"
+      v-model.trim="newTag"
+      @keydown.enter="addNewTag"
+      @keydown.delete="removeLastTag"
+      @keydown.tab.prevent="addNewTag"
+      :class="{ 'tags-exists': isTagExists }"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    tags: [...this.selectedTags],
+    newTag: "",
+  }},
+  props: {
+    selectedTags: {
+      type: Array,
+      default: () => [],
+      
+    },
+  },
+  computed: {
+    isTagExists() {
+      return this.tags.includes(this.newTag);
+    },
+  },
+  emits: ["change"],
+  methods: {
+    addNewTag() {
+      if (this.newTag && !this.isTagExists) {
+        this.tags.push(this.newTag);
+        this.newTag = "";
+        this.$emit("change",this.tags);
+      }
+    },
+    removeTag(index) {
+      this.tags.splice(index, 1);
+      this.$emit("change",this.tags);
+    },
+    removeLastTag() {
+      if (this.newTag.length === 0) this.removeTag(this.tags.length - 1);
+    },
+  },
+  watch: {
+    newTag(newVal, oldVal) {
+      if (newVal.indexOf(",") > -1) {
+        this.newTag = this.newTag.slice(0, -1);
+        this.addNewTag();
+      }
+    },
+  },
+};
+</script>
+
+<style scoped>
+.tag-input.tag-exists {
+  color: red;
+  text-decoration: line-through;
+}
+
+.tags-input-wrapper {
+  background: #fff;
+  padding: 0.5em;
+  border: 1px solid #dbdbdb;
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  min-height: 36px;
+  box-sizing: border-box;
+}
+.tag-item {
+  color: #212529;
+  background-color: #eee;
+  margin-right: 0.3em;
+  padding: 0.25em 0.4em;
+  font-size: 75%;
+  line-height: 1;
+  text-align: center;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  padding-right: 1.25em;
+  padding-left: 0.6em;
+}
+.tag-input {
+  color: #495057;
+  flex: 1;
+  background: transparent;
+  border: none;
+}
+.tag-input:focus {
+  outline: none;
+}
+a.remove-tag {
+  text-decoration: none;
+}
+</style>
